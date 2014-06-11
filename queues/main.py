aec4fc01f412f1e65c1f745f1c68da70aa0824ce
mario.souza <mario.souza@walmart.com>
2014-06-11 16:19:43 -0300

    initiating queues with descriptive introduction

---
+++ queues/main.py
@@ -0,0 +1,19 @@
+"""
+  Let's take a look at two ADTs:
+  - Queue
+  - Priority Queue
+
+  Real life examples:
+  - in most cases, the first customer in line is the next customer to be served
+  - at supermarkets, a customer with only a few items go FIRST
+  - at airports, customers whose flights are leaving soon are taken from the middle of the queue
+
+  
+  queueing policy
+  - the rule that determines who goes next
+  - e.g.: FIFO, LIFO, priority queueing
+
+  priority queueing
+  - each customer is assigned a priority and the customer with the highest priority goes first, regardless of the order of arrival
+
+"""
