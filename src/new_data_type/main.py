98af71eee63cc58923edfc59e3ee798346213075
mario.souza <mario.souza@walmart.com>
2014-06-16 21:07:28 -0300

    learning how to build a new data type

---
+++ src/new_data_type/main.py
@@ -0,0 +1,13 @@
+from fraction import Fraction
+
+print Fraction(5, 6) + Fraction(2, 3)
+print Fraction(5, 6) + 10
+print 20 + Fraction(2, 3)
+
+print Fraction(5, 6) * Fraction(2, 3)
+print Fraction(5, 6) * 10
+print 20 * Fraction(2, 3)
+
+print Fraction(100,-36)
+
+print Fraction(20) == Fraction(20)
