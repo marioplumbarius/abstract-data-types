0c4f7a33ac2466efb9631711048841a8eb25f690
Mario Luan <mariosouzaluan@gmail.com>
2014-06-23 00:15:17 -0300

    creating the deque adt interface

---
+++ src/deque/deque_interface.py
@@ -0,0 +1,30 @@
+class DequeInterface():
+	""" interface of a double-ended queue adt """
+
+	def __init__(self):
+		""" create an empty deque """
+		pass
+
+	def insertFirst(self, item):
+		""" add a new item at the end of the deque """
+		pass
+	
+	def insertLast(self, item):
+		""" add a new item at the top of the deque """
+		pass
+
+	def removeFirst(self):
+		""" remove and return the item at the top of the deque """
+		pass
+
+	def removeLast(self):
+		""" remove and return the item at the end of the deque """
+		pass
+	
+	def isEmpty(self):
+		""" check wether the deque is empty """
+		pass
+
+	def size(self):
+		""" return the length of the deque """
+		pass
