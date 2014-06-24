c7d1d832a439ee29c689e4f6226cd2d55f593f91
mario.souza <mario.souza@walmart.com>
2014-06-24 10:04:50 -0300

    raising when interface is not implemented properly

--- python/src/deque/deque_interface.py
+++ python/src/deque/deque_interface.py
@@ -3,28 +3,28 @@ class DequeInterface():
 
 	def __init__(self):
 		""" create an empty deque """
-		pass
+		raise "DequeInterface.__init__ is not implemented"
 
 	def insertFirst(self, item):
 		""" add a new item at the end of the deque """
-		pass
+		raise "DequeInterface.insertFirst is not implemented"
 	
 	def insertLast(self, item):
 		""" add a new item at the top of the deque """
-		pass
+		raise "DequeInterface.insertLast is not implemented"
 
 	def removeFirst(self):
 		""" remove and return the item at the top of the deque """
-		pass
+		raise "DequeInterface.removeFirst is not implemented"
 
 	def removeLast(self):
 		""" remove and return the item at the end of the deque """
-		pass
+		raise "DequeInterface.removeLast is not implemented"
 	
 	def isEmpty(self):
 		""" check wether the deque is empty """
-		pass
+		raise "DequeInterface.isEmpty is not implemented"
 
 	def size(self):
 		""" return the length of the deque """
-		pass
+		raise "DequeInterface.size is not implemented"
