63abba231b0939271aa07b425e6a24b492e1bc2b
mario.souza <mario.souza@walmart.com>
2014-06-24 10:05:15 -0300

    creating docstring for deque implementation methods

--- python/src/deque/deque_list.py
+++ python/src/deque/deque_list.py
@@ -4,22 +4,29 @@ class DequeList(DequeInterface):
 	""" implementation of a deque using a python list """
 
 	def __init__(self):
+		""" create an empty deque """
 		self.items = []
 
 	def isEmpty(self):
+		""" check if the deque is empty """
 		return (self.items == [])
 
 	def insertFirst(self, item):
+		""" add an item at the top of the deque """
 		self.items.insert(0, item)
 
 	def insertLast(self, item):
+		""" add an item at the end of the deque """
 		self.items.append(item)
 
 	def removeFirst(self):
+		""" remove and return the item at the top of the deque """
 		return self.items.pop(0)
 
 	def removeLast(self):
+		""" remove and return the item at the end of the deque """
 		return self.items.pop()
 
 	def size(self):
+		""" return the length of the deque """
 		return len(self.items)
