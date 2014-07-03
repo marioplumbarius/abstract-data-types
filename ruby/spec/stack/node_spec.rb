21ee9c127d11568b6adc2722b84d5c35cee0db51
Mario Luan <mariosouzaluan@gmail.com>
2014-07-02 22:47:26 -0300

    created Node spec

---
+++ ruby/spec/stack/node_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+require 'stack/node'
+
+describe Node do
+
+  describe '#new' do
+    subject { Node.new cargo, nextt }
+    let(:cargo) { 1 }
+    let(:nextt) { Node.new(cargo*2) }
+
+    it 'assigns @cargo' do
+      expect(subject.cargo).to eq(cargo)
+    end
+
+    it 'assigns @nextt' do
+      expect(subject.nextt).to eq(nextt)
+      expect(subject.nextt).to be_a Node
+    end
+
+    it 'includes Comparable module' do
+      expect(described_class).to include(Comparable)
+    end
+  end
+
+  describe '#to_s' do
+    subject { Node.new cargo }
+    let(:cargo) { 22 }
+    
+    it 'puts its @cargo value' do
+      expect(subject.to_s).to match(/cargo: #{cargo}/)
+    end
+  end
+
+  describe '#<=>' do
+    let(:node_greater) { Node.new cargo_greater }
+    let(:node_lesser) { Node.new cargo_lesser }
+    let(:cargo_greater) { 100 }
+    let(:cargo_lesser) { 50 }
+
+    it 'implements the comparisson using the @cargo attribute' do
+      expect(node_greater > node_lesser).to eq(cargo_greater > cargo_lesser)
+    end
+  end
+
+end
